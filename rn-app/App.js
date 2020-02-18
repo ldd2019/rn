5fe6725a51aab4b0d2a2c30835d8ace31f67a3a8
ldd2019 <616270735@qq.com>
2020-02-18 12:23:13 +0800

    navigate测试

---
+++ rn-app/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import AppWithNavigationState from './src/navigator/AppNavigators'; // 入口文件
+
+export default function App() {//根组件，整个app的入口
+  return (
+    <AppWithNavigationState/>
+  );
+}
+// In App.js in a new project
